b587a62a85ded461282dd74ed00fc97239190e6e
Daniel Borisenko <danib564@gmail.com>
2024-04-22 14:48:33 +0300

    added upload tuki component

---
+++ taas-frontend/src/components/UploadTuki.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../App.css";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Upload tuki</div>;
+  }
+}
+
+export default Navbar;
