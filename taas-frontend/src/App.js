06ee0d2781465a4cb03a26bd9b09c2850e6b14e8
Daniel Borisenko <danib564@gmail.com>
2024-04-22 14:48:10 +0300

    Added basic navbar with link and logo

--- taas-frontend/src/App.js
+++ taas-frontend/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import GetTuki from "./components/GetTuki";
+import UploadTuki from "./components/UploadTuki";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/get-tuki" element={<GetTuki />} />
+        <Route exact path="/Upload-tuki" element={<UploadTuki />} />
+      </Routes>
+    </Router>
   );
 }
 
