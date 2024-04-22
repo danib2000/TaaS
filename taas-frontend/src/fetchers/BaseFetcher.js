0449dd9dbea384e4110a0efd6aba1f3e5d04e352
Daniel Borisenko <danib564@gmail.com>
2024-04-22 15:23:13 +0300

    Added fetchers and axios

---
+++ taas-frontend/src/fetchers/BaseFetcher.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+class BaseFetcher {
+  static baseUrl = "http://localhost:3001";
+
+  static async httpget(url) {
+    return await axios.get(this.baseUrl + url);
+  }
+  static async httpPost(url, body) {
+    return await axios.post(this.baseUrl + url, body);
+  }
+}
+
+export default BaseFetcher;
---
+++ taas-frontend/src/fetchers/TukiFetcher.js
@@ -0,0 +1,17 @@
+import BaseFetcher from "./baseFetcher";
+
+class TukiFetcher extends BaseFetcher {
+  static routerBaseUrl = "/Tukis";
+
+  static async getTuki() {
+    return (await this.httpget(this.routerBaseUrl)).data;
+  }
+  static async postProduct(name, type, imageSrc) {
+    return this.httpPost(this.routerBaseUrl, {
+      name: name,
+      type: type,
+      imgageSrc: imageSrc,
+    });
+  }
+}
+export default TukiFetcher;
