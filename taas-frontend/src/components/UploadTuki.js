cd0ddcbc1a17b99657afa99e2b47aa1547f3f2cd
Daniel Borisenko <danib564@gmail.com>
2024-04-22 16:36:27 +0300

    Added upload tuki form

--- taas-frontend/src/components/UploadTuki.js
+++ taas-frontend/src/components/UploadTuki.js
@@ -1,15 +1,95 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component, useState, useEffect } from "react";
 import "../App.css";
+import "./UploadTuki.css";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-  }
+const UploadTuki = () => {
+  const [image, setImage] = useState();
+  const [tukiName, setTukiName] = useState();
+  const [tukiType, setTukiType] = useState();
 
-  render() {
-    return <div>Upload tuki</div>;
-  }
-}
+  const handleNameChange = (e) => {
+    setTukiName(e.target.value);
+  };
 
-export default Navbar;
+  const handleTypeChange = (e) => {
+    setTukiType(e.target.value);
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(tukiName);
+    console.log(tukiType);
+    console.log(image);
+    // Handle form submission here
+  };
+
+  // useEffect(() => {
+  //   if (uploadedFile) {
+  //     console.log(uploadedFile);
+  //   }
+  //   if (tukiName) {
+  //     console.log(tukiName);
+  //   }
+  //   // Do something
+  // }, [uploadedFile, tukiName]);
+
+  // const uploadFile = (e) => {
+  //   console.log(uploadedFile);
+  // };
+
+  // return (
+  //   <div className="App">
+  //     <div>
+  //       <input
+  //         type="file"
+  //         onChange={(e) => setUploadedFile(e.target.files[0])}
+  //       />
+  //       <input type="text" onChange={(e) => setTukiName(e.target.value)} name />
+  //       <button onClick={uploadFile}>Upload</button>
+  //     </div>
+  //   </div>
+  // );
+
+  return (
+    <div className="input-form-container">
+      {/* <h2>Input Form</h2> */}
+      <form onSubmit={handleSubmit} className="input-form">
+        <div className="input-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={tukiName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="type">Type:</label>
+          <input
+            type="text"
+            id="type"
+            value={tukiType}
+            onChange={handleTypeChange}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="image">Image:</label>
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadTuki;
