f6bdb1ca811fb3dae890607532a2fc0afbb840b8
Daniel Borisenko <danib564@gmail.com>
2024-04-22 14:48:23 +0300

    Added get tuki component

---
+++ taas-frontend/src/components/GetTuki.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../App.css";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div> Get Tuki</div>;
+  }
+}
+
+export default Navbar;
