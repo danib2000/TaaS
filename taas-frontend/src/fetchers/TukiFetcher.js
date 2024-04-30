991a3c685fb0377e94e4b3e3d951003a1a91c577
Daniel Borisenko <danib564@gmail.com>
2024-04-30 22:41:38 +0300

    Added get tuki by name and type

--- taas-frontend/src/fetchers/TukiFetcher.js
+++ taas-frontend/src/fetchers/TukiFetcher.js
@@ -1,11 +1,20 @@
-import BaseFetcher from "./baseFetcher";
+import BaseFetcher from "./BaseFetcher";
 
 class TukiFetcher extends BaseFetcher {
   static routerBaseUrl = "/Tukis";
 
-  static async getTuki() {
-    return (await this.httpget(this.routerBaseUrl)).data;
+  static async getTukis() {
+    return (await this.httpGet(this.routerBaseUrl)).data;
   }
+
+  static async getTukisByName(name) {
+    return await this.httpGet(this.routerBaseUrl + "/name/" + name);
+  }
+
+  static async getTukisByType(type) {
+    return await this.httpGet(this.routerBaseUrl + "/type/" + type);
+  }
+
   static async postTuki(name, type, imageSrc) {
     return this.httpPost(this.routerBaseUrl, {
       name: name,
