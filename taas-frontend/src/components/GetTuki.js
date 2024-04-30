069d10e55c8a0ddb5a8a21c68bec5ad9c42df307
Daniel Borisenko <danib564@gmail.com>
2024-04-30 22:46:04 +0300

    Added slider and backend request

--- taas-frontend/src/components/GetTuki.js
+++ taas-frontend/src/components/GetTuki.js
@@ -2,12 +2,40 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./GetTuki.css";
 import "../App.css";
+import tukiFetcher from "../fetchers/TukiFetcher";
+import Slider from "react-slick"; // Assuming you're using react-slick for the carousel
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const GetTuki = () => {
   const [searchType, setSearchType] = useState("");
   const [searchName, setSearchName] = useState("");
   const [searchBy, setSearchBy] = useState("name"); // Default to searching by name
   const [visibleSearch, setVisibleSearch] = useState("unset");
+  const [tukiImagess, setTukiImagess] = useState("");
+  const [visibleTuki, setVisibleTuki] = useState("none");
+  const [searchErrorDisplay, setSearchErrorDisplay] = useState("none");
+
+  const tukiImages = [
+    "https://taas-cool-bucket.s3.amazonaws.com/tuki1.jpeg",
+    "https://taas-cool-bucket.s3.amazonaws.com/tuki2.jpeg",
+    // "https://taas-cool-bucket.s3.amazonaws.com/tuki3.jpeg",
+    // "https://taas-cool-bucket.s3.amazonaws.com/tuki4.jpeg",
+    // "https://taas-cool-bucket.s3.amazonaws.com/parrot.png",
+  ];
+
+  // To DO add dynamic Tuki carousal with backend request
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+  };
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -18,7 +46,40 @@ const GetTuki = () => {
       "and name:",
       searchName
     );
+
+    setSearchErrorDisplay("none");
+
+    if (!searchName && !searchType) {
+      setSearchErrorDisplay("unset");
+    }
+
     //setVisibleSearch("none");
+    if (searchBy === "name") {
+      console.log("name");
+
+      tukiFetcher
+        .getTukisByName(searchName)
+        .then((res) => {
+          setTukiImagess(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else if (searchBy === "type") {
+      tukiFetcher
+        .getTukisByType(searchType)
+        .then((res) => {
+          setTukiImagess(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    // request backend
+
+    setVisibleTuki("unset");
+
     // You can add your search logic here
   };
 
@@ -63,10 +124,41 @@ const GetTuki = () => {
               placeholder={`Enter ${searchBy === "name" ? "name" : "type"}`}
             />
           </div>
+          <p style={{ display: searchErrorDisplay, color: "red" }}>
+            {"You need to enter something to search!"}
+          </p>
           <button type="submit" className="search-button">
             Search
           </button>
         </form>
+      </div>
+      <div
+        style={{ textAlign: "center", padding: "20px", display: visibleTuki }}
+      >
+        <h1>Your requested Tuki:</h1>
+        <div style={{ width: "70%", margin: "auto" }}>
+          <Slider {...settings}>
+            {tukiImages.map((image, index) => (
+              <div key={index} style={{ textAlign: "center" }}>
+                <img
+                  src={image}
+                  alt={image}
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "1000px",
+                    display: "block",
+                    margin: "0 auto",
+                    height: "300px",
+                  }}
+                />
+                <div>
+                  <h2>"tuki"</h2>
+                  <p>"asdsad</p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
