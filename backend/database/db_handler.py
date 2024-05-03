b9b4a32fa437e2b328e0cdb3018bcf4e957fadac
shirazkl <shirazkl18@gmail.com>
2024-05-03 21:55:26 +0300

    Add functions that get tukis from db by name/type and delete by name/type

--- backend/database/db_handler.py
+++ backend/database/db_handler.py
@@ -26,8 +26,77 @@ class DBHandler:
 
         return tukis
 
+    def get_tuki_by_name(self, name):
+        """
+        Retrieve Tuki records from the database by name.
+        """
+        with self.session() as session:
+            return session.query(Tuki).filter(Tuki.name == name).all()
+
+    def get_tuki_by_type(self, type):
+        """
+        Retrieve Tuki records from the database by type.
+        """
+        with self.session() as session:
+            return session.query(Tuki).filter(Tuki.type == type).all()
+
+    def get_tuki_by_name_and_type(self, name, type):
+        """
+        Retrieve Tuki records from the database by name and type.
+        """
+        with self.session() as session:
+            return session.query(Tuki).filter(Tuki.type == type, Tuki.name == name).all()
+
+    def delete_tuki_by_name(self, name):
+        """
+        Delete Tuki records from the database by name.
+        """
+        with self.session() as session:
+            deleted_count = session.query(Tuki).filter(Tuki.name == name).delete()
+            session.commit()
+
+            return deleted_count
+
+    def delete_tuki_by_type(self, type):
+        """
+        Delete Tuki records from the database by type.
+        """
+        with self.session() as session:
+            deleted_count = session.query(Tuki).filter(Tuki.type == type).delete()
+            session.commit()
+
+            return deleted_count
+
+    def delete_tuki_by_name_and_type(self, name, type):
+        """
+        Delete Tuki records from the database by name and type.
+        """
+        with self.session() as session:
+            deleted_count = session.query(Tuki).filter(Tuki.name == name, Tuki.type == type).delete()
+            session.commit()
+
+            return deleted_count
+
 
 if __name__ == '__main__':
     db_handler = DBHandler(DB_URL)
     # db_handler.create_tables()
     # print("tables created successfully")
+    db_handler.add_tuki("yosi", "kon", "img.src")
+    x = db_handler.get_tuki_by_name("yosi")
+    print(x)
+
+    x = db_handler.get_tuki_by_type("green cheek")
+    print(x)
+
+    x = db_handler.get_tuki_by_type("kon")
+    print(x)
+
+    x = db_handler.get_tuki_by_name_and_type("moshon", "green cheek")
+    print(x)
+
+    x = db_handler.delete_tuki_by_name_and_type("yosi","kon")
+    print(x)
+
+    x = db_handler.get_tuki_by_name_and_type("yosi","kon")
+    print(x)
