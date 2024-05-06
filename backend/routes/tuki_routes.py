a9b23840a5d15b741c718da9856c114fc0daa738
shirazkl <shirazkl18@gmail.com>
2024-05-06 16:58:03 +0300

    Add route for get tukis by type

--- backend/routes/tuki_routes.py
+++ backend/routes/tuki_routes.py
@@ -11,16 +11,16 @@ db_handler = DBHandler(DB_URL)
 @router.get("/")
 def get_all_tukis():
     """
-        Retrieve all tukis.
+    Retrieve all tukis.
 
-        Returns:
-            dict: A dictionary containing the tukis list as value of data key.
+    Returns:
+        dict: A dictionary containing the tukis list as value of data key.
 
-        Raises:
-            HTTPException: If no Tukis are found for the specified name, a 404 HTTPException is raised.
-                           If there's a database error, a 500 HTTPException is raised.
-                           For any other unexpected exceptions, a 500 HTTPException is raised.
-                           All the exceptions are raised with a detail message.
+    Raises:
+        HTTPException: If no Tukis are found for the specified name, a 404 HTTPException is raised.
+                       If there's a database error, a 500 HTTPException is raised.
+                       For any other unexpected exceptions, a 500 HTTPException is raised.
+                       All the exceptions are raised with a detail message.
     """
     try:
         tukis = db_handler.get_all_tukis()
@@ -38,19 +38,19 @@ def get_all_tukis():
 @router.get("/name/{name}")
 def get_tukis_by_name(name: str):
     """
-        Retrieve Tukis by name.
+    Retrieve Tukis by name.
 
-        Args:
-            name (str): The name of the tuki to retrieve.
+    Args:
+        name (str): The name of the tuki to retrieve.
 
-        Returns:
-            dict: A dictionary containing the tukis list as value of 'data' key.
+    Returns:
+        dict: A dictionary containing the tukis list as value of 'data' key.
 
-        Raises:
-            HTTPException: If no Tukis are found for the specified name, a 404 HTTPException is raised.
-                           If there's a database error, a 500 HTTPException is raised.
-                           For any other unexpected exceptions, a 500 HTTPException is raised.
-                           All the exceptions are raised with a detail message.
+    Raises:
+        HTTPException: If no Tukis are found for the specified name, a 404 HTTPException is raised.
+                       If there's a database error, a 500 HTTPException is raised.
+                       For any other unexpected exceptions, a 500 HTTPException is raised.
+                       All the exceptions are raised with a detail message.
     """
     try:
         tukis = db_handler.get_tukis_by_name(name)
@@ -64,3 +64,32 @@ def get_tukis_by_name(name: str):
 
     return {"data": tukis}
 
+
+@router.get("/type/{type}")
+def get_tukis_by_type(type: str):
+    """
+    Retrieve Tukis by type.
+
+    Args:
+        type (str): The type of the tukis to retrieve.
+
+    Returns:
+        dict: A dictionary containing the tukis list as value of 'data' key.
+
+    Raises:
+        HTTPException: If no Tukis are found for the specified type, a 404 HTTPException is raised.
+                       If there's a database error, a 500 HTTPException is raised.
+                       For any other unexpected exceptions, a 500 HTTPException is raised.
+                       All the exceptions are raised with a detail message.
+    """
+    try:
+        tukis = db_handler.get_tukis_by_type(type)
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    if not tukis:
+        raise HTTPException(status_code=404, detail=f"Tukis not found for type - {type}")
+
+    return {"data": tukis}
